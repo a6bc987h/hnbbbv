0786dac53a05f4bfddf640c637521553c270670d
a6bc987h <43772681+a6bc987h@users.noreply.github.com>
2018-10-04 19:12:58 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -21,38 +21,33 @@ PM_START_TEXT = """
 
 Hai {}, 
 My name is {}! 
-I'm a group manager bot maintained by [this noob](tg://user?id={}).
-I'm built in python3, using the python-telegram-bot library, and am fully opensource you can find what makes me tick [here](https://github.com/PaulSonOfLars/tgbot)!
-You can find the list of available commands with /help.
+I'm a group manager bot maintained by [this noob](t.me/TonyJr_Online).
+I'm here to help you manage your groups! Hit /help to find out more about how to use me to my full potential.
 For all your doubts about telegram use [TG_Helpbot](https://t.me/tg_helpbot) ..
 Using standard [Marie](http://t.me/BanhammerMarie_bot) is always gonna be easier.
 You can see my updates [here](https://t.me/TonyJr_Online).
 
 
-[Click here to add my clones in your groups](https://telegram.me/tonyjr_bot?startgroup=add)
+[Click here to add me in your groups](https://telegram.me/tonyjr_bot?startgroup=add)
 """
 
 HELP_STRINGS = """
 Hey there! My name is *{}*.
 I'm a modular group management bot with a few fun extras! Have a look at the following for an idea of some of the things I can help you with.
 
-Main commands available:
- - /start: start the bot
- - /help: PM's you this message.
- - /help <module name>: PM's you info about that module.
- - /donate: information about how to donate!
- - /settings:
-   - in PM: will send you your settings for all supported modules.
-   - in a group: will redirect you to pm, with all that chat's settings.
+A few handy commands are:
+ - /start: starts me! You've probably already used this.
+ - /help: gets this message out; I'll tell you more about myself!
+ - /donate: gives you info on how to support me and my creator.
 
 
 And the following:
 """.format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
 
 DONATE_STRING = """Heya, glad to hear you want to donate!
-It took lots of work for [my creator](t.me/sonoflars) to get me to where I am now, and every donation helps \
+It took lots of work for [my creator](t.me/tonyjr_online) to get me to where I am now, and every donation helps \
 motivate him to make me even better. 
-Contact [him](http://t.me/sonoflars) for donate me .... Thank you"""
+[Click here](https://telegra.ph/Donate-10-04) for donate me .... Thank you"""
 
 IMPORTED = {}
 MIGRATEABLE = []
